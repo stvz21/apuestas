74b1636409d5510d2c2d60bbccb05d797493240c
stvz21 <126835371+stvz21@users.noreply.github.com>
2023-03-14 15:47:45 -0400

    Update notifcar.py

--- notifcar.py
+++ notifcar.py
@@ -3,7 +3,7 @@ import telebot
 class Notificar:
     estado = True
     def __init__(self):
-        self.bot = telebot.AsyncTeleBot("1238627815:AAGBjaRN0CxD6zsCTg0tyJSOBt_lw6GZU5M", parse_mode='MARKDOWN')
+        self.bot = telebot.AsyncTeleBot("5871277082:AAFsEc0clhaeJ0wokJVfGF0_P3P0385Sb0M", parse_mode='MARKDOWN')
         # @self.bot.message_handler(commands=['start'])
         # def send_welcome(message):
         #     self.process_command_start(message)
